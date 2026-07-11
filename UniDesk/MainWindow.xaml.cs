4f4c38b2682456d16d6b56452c7d03e77ada0e46
SuperDaddyV <noreply@github.com>
2026-07-11 22:09:04 +0800

    feat: refine main window glass hierarchy

--- UniDesk/MainWindow.xaml.cs
+++ UniDesk/MainWindow.xaml.cs
@@ -37,6 +37,8 @@ public partial class MainWindow : Window
 
         AppIconHelper.ApplyWindowIcon(this);
         DesktopWidgetWindowHelper.Configure(this);
+        SourceInitialized += (_, _) =>
+            BackdropMaterialService.Apply(this, BackdropKind.MainWindow);
 
         ApplyInitialWindowBounds();
         _viewModel.PropertyChanged += ViewModel_OnPropertyChanged;
